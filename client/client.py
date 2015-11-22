089ea76e312a741407bb8b0d35828da962e89cc5
mitchfriedman <mitchfriedman5@gmail.com>
2015-11-21 18:04:39 -0600

    Update client

--- client/client.py
+++ client/client.py
@@ -5,17 +5,16 @@ import random
 
 class RequestGenerator(object):
     
-    endpoints = {
-        '/api/Users': 0.1,
-        '/api/Authentication': 0.2,
-        '/api/Posts': 0.3,
-        '/api/Logout': 0.4,
-    }
+    endpoints = [
+        ('/api/users', 0.1),
+        ('/api/authentication', 0.2),
+        ('/api/posts', 0.3),
+        ('/api/logout', 0.4),
+    ]
 
     def __init__(self, uri):
         self.uri = uri
-        self.probabilities = [v for v in self.endpoints.values()]
-        self.reverse_endpoint_mappings = {v:k for k,v in self.endpoints.items()}
+        self.probabilities = [p for e,p in self.endpoints]
 
     def generate_random_endpoint(self):
         totals = list(itertools.accumulate(self.probabilities))
@@ -23,14 +22,15 @@ class RequestGenerator(object):
         n = random.uniform(0, totals[-1])
         for i, total in enumerate(totals):
             if n <= total:
-                return float(i)/10
+                return i
 
     def make_random_requests(self, num_requests):
         i=0
         while i < num_requests:
-            random_uri = self.reverse_endpoint_mappings[self.generate_random_endpoint()]
+            random_uri = self.endpoints[self.generate_random_endpoint()][0]
             resp = requests.get(self.uri + random_uri)
             print(resp)
+            i += 1
 
 if __name__ == '__main__':
     generator = RequestGenerator('http://45.79.134.56')
